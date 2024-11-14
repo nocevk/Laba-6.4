3f57bd56e566094002daaf01faf16561e636042a
Kirilo Chori <kirilosakanaliev@gmail.com>
2024-11-14 18:42:27 +0200

    Добавьте файлы проекта.

---
+++ Laba 6.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_6._4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введіть послідовність чисел (0 для завершення):");
+            ReverseSequence();
+        }
+
+        static void ReverseSequence()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                ReverseSequence(); 
+                Console.WriteLine(number); 
+            }
+        }
+    }
+}
+
+
+
+
